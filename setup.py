85c8524c40d2a1040105d16f7a339928264b9c4d
Sergey B Kirpichev <skirpichev@gmail.com>
2019-04-21 17:49:37 +0300

    Move setuptools metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -1,24 +1,6 @@
-from setuptools import setup
+"""Setup script for gmpy_ctypes."""
 
-setup(
-    name='gmpy_ctypes',
-    version='0.1',
-    author="Sergey B Kirpichev",
-    author_email="skirpichev@gmail.com",
-    license="LGPL-3.0+",
-    description="GMP ctypes wrapper",
-    keywords=["PyPy", "GMP"],
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)',
-        'Natural Language :: English',
-        'Operating System :: POSIX',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Scientific/Engineering :: Mathematics',
-    ],
-    packages=['gmpy_ctypes'],
-    zip_safe=True)
+import setuptools
+
+
+setuptools.setup()
